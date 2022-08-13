5f1f178243c98c882fca7289a72758417ea0c475
Akachai <akachai1526@hotmail.com>
2022-08-13 12:03:59 +0700

    Add the remaining file

---
+++ public/custom/js/custom_index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    init_sparklines();
+    init_flot_chart();
+    init_sidebar();
+    init_InputMask();
+    init_daterangepicker();
+    init_daterangepicker_right();
+    init_daterangepicker_single_call();
+    init_daterangepicker_reservation();
+    init_skycons();
+    init_select2();
+    init_PNotify();
+    init_compose();
+    init_CustomNotification();
+    init_autosize();
+    init_autocomplete();
+
+});

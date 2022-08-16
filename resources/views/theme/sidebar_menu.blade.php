87ec1285cf68525402f6f4c4b2a8e5b8fab8d771
Akachai <akachai1526@hotmail.com>
2022-08-16 17:08:59 +0700

    Add the remaining file

--- resources/views/theme/sidebar_menu.blade.php
+++ resources/views/theme/sidebar_menu.blade.php
@@ -3,7 +3,7 @@ $user = auth()->user();
 @endphp
  <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
     <div class="menu_section">
-      <h3>Menu</h3>
+      <h3>ระบบจัดการบทความ</h3>
       <ul class="nav side-menu" >
 
         <li class="">

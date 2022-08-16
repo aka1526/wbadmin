93219f448f4c2df70ccb6181cab6577b8a787297
Akachai <akachai1526@hotmail.com>
2022-08-16 17:07:22 +0700

    Add the remaining file

--- resources/views/theme/main.blade.php
+++ resources/views/theme/main.blade.php
@@ -37,7 +37,8 @@
 			<div class="col-md-3 left_col">
 				<div class="left_col scroll-view">
 					<div class="navbar nav_title" style="border: 0;">
-						<a href="/" class="site_title"><i class="fa fa-paw"></i> <span>Document Control</span></a>
+						<a href="/" class="site_title"><i class="fa fa-paw"></i>
+                            <span>p-quality</span></a>
 					</div>
 
 					<div class="clearfix"></div>

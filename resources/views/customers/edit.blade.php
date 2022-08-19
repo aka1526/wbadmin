b170659f6653d20fa6e9084617ed5c946221750f
Akachai <akachai1526@hotmail.com>
2022-08-19 17:32:19 +0700

    Add the remaining file

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -36,6 +36,55 @@
             display: none;
         }
     </style>
+    <style type="text/css">
+        #table-2 {
+            border: 1px solid #e3e3e3;
+            background-color: #66A3FF;
+                width: 874px;
+            border-radius: 6px;
+            -webkit-border-radius: 6px;
+            -moz-border-radius: 6px;
+        }
+        #table-2 td, #table-2 th {
+            padding: 5px;
+            color: #333;
+        }
+         #table-2 th {
+            padding: 5px;
+            color: #ffffff;
+        }
+        #table-2 thead {
+            font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+            padding: .2em 0 .2em .5em;
+            text-align: left;
+            color: #4B4B4B;
+            background-color: #2EB8E6;
+            background-image: -webkit-gradient(linear, left top, left bottom, from(#f2f2f2), to(#e3e3e3), color-stop(.6,#B3B3B3));
+            background-image: -moz-linear-gradient(top, #D6D6D6, #B0B0B0, #B3B3B3 90%);
+            border-bottom: solid 1px #999;
+        }
+        #table-2 th {
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            background-color: #00248F;
+            font-size: 17px;
+            line-height: 20px;
+            font-style: normal;
+            font-weight: normal;
+            text-align: center;
+            text-shadow: white 1px 1px 1px;
+        }
+        #table-2 td {
+            line-height: 20px;
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            background-color: #B2D1FF;
+            font-size: 14px;
+            border-bottom: 1px solid #fff;
+            border-top: 1px solid #fff;
+        }
+
+
+        }
+        </style>
 @endsection
 
 @section('page_content')
